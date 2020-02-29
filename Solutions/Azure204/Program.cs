6452aa5395721c647d635fc6d37f888aecd5e27e
sumitjoshi <joshisumitjoshi@gmail.com>
2020-02-29 11:18:44 +0530

    move

---
+++ Solutions/Azure204/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Azure204
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
